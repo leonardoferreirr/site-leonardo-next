129cdfc63a18cd39372f731718e980ba0026561c
Leonardo Ferreira <leonardoferreirrs@gmail.com>
2026-03-17 11:47:35 -0300

    Instala Meta Pixel globalmente via layout base

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,8 +26,26 @@ export default async function LocaleLayout(props: {
       <head>
         {/* Feather Icons CDN */}
         <script src="https://unpkg.com/feather-icons"></script>
+
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2116057592584389');
+          fbq('track', 'PageView');
+        ` }} />
+        {/* End Meta Pixel Code */}
       </head>
       <body id="top">
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=2116057592584389&ev=PageView&noscript=1" alt="" />
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
